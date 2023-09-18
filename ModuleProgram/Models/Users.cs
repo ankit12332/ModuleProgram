736a9f71d546a2bbd0394fb7204e489dc43d163c
ankit12332 <ankit.panda010.ap@gmail.com>
2023-09-18 11:44:56 +0530

    Added User Module

---
+++ ModuleProgram/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModuleProgram.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50)]
+        public string? username { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50)]
+        public string? password { get; set; }
+    }
+}
