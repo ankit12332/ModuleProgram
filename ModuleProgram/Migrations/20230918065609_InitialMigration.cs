b6ae3c5d2d62fd1bd4fd36ea181d15d5b0a969a9
ankit12332 <ankit.panda010.ap@gmail.com>
2023-09-21 09:53:09 +0530

    Updated UserEntityModel

---
+++ ModuleProgram/Migrations/20230918065609_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuleProgram.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ModuleProgram/Models/User.cs
+++ ModuleProgram/Models/User.cs
@@ -12,11 +12,13 @@ namespace ModuleProgram.Models
 
         [Required(ErrorMessage = "Username is required.")]
         [StringLength(50)]
-        public string? username { get; set; }
+        public string? Username { get; set; }
 
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(50)]
-        public string? password { get; set; }
+        public string? Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
