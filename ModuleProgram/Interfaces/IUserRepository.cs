da9ab696d7ce9454c4ba7022297c4c39e2404c7b
ankit12332 <ankit.panda010.ap@gmail.com>
2023-09-21 10:47:56 +0530

    Added IUserRepository and UserRepository

---
+++ ModuleProgram/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ModuleProgram.Models;
+
+namespace ModuleProgram.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateUserAsync(User user);
+        Task<User> GetUserByIdAsync(int id);
+        Task<List<User>> GetAllUsersAsync();
+        Task UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
---
+++ ModuleProgram/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuleProgram.Context;
+using ModuleProgram.Interfaces;
+using ModuleProgram.Models;
+
+namespace ModuleProgram.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                return new User();
+            }
+
+            return user;
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
